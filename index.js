a6b6b7ee17572ccb8bc19af1591330e25571290d
lmccodes <lisa.m.childress@gmail.com>
2020-04-25 22:11:08 -0700

    hour

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ let swap = function() {
   a = c;
   console.log(a,b);
 }
+
+let hour = 10;
+
+if (hour >= 6 && hour < 12) {
+  console.log('Good morning!');
+}
+else if (hour >= 12 && hour < 18) {
+  console.log('Good afternoon');
+}
+else {
+  consol.log('Good evening');
+}
